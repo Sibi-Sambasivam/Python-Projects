69a328fe8a0c3aa1cd01425aa3ab412d5fdc6b19
Sibi Sambasivam <sibisambasivam3@gmail.com>
2024-04-22 21:37:14 +0200

    Add SMS Sender

--- SMSsender.py
+++ SMSsender.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = 'ACdc0cc880c06289995d56b0e9b7908c8c'
-auth_token = '6436c5c349b0281ceac0b06ab4c7e65f'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
