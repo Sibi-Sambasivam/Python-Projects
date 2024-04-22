0529265cb4988a89d9c9a1893c763f5b2e911e1a
Sibi Sambasivam <sibisambasivam3@gmail.com>
2024-04-22 21:37:14 +0200

    Add SMS Sender

--- SMSsender.py
+++ SMSsender.py
@@ -1,12 +1,13 @@
+#from twilio.rest import Client
 
 #account_id = ''
 #au_token = ''
 #client = Client(account_id, au_token)
 
 #message = client.messages.create(
-  from ='+12513222845',
-  body ='This SMS was sent using Python',
+ # from ='A Phone number that is given to you or a from number',
+ # body ='This SMS was sent using Python',
 # to = """Write your Phone Number here"""
-)
+#)
 
 #print(message.id)
