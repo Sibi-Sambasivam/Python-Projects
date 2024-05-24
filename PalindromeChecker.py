1e4c392e8d29e0c651b56d8a311c5da2b50b40c7
Sibi Sambasivam <sibisambasivam3@gmail.com>
2024-05-24 16:42:40 +0530

    Add PalindromeChecker

---
+++ PalindromeChecker.py
@@ -0,0 +1,8 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("Palindrome!")
+else:
+    print("Not a palindrome.")
